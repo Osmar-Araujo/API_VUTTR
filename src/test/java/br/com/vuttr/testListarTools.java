7cdf544e83814a264f47e063d575b2720f73e1be
Osmar-Araujo <araujoo@gmail.com>
2021-04-02 20:51:24 -0300

    Ajustando testes

--- src/test/java/br/com/vuttr/testListarTools.java
+++ src/test/java/br/com/vuttr/testListarTools.java
@@ -1,8 +1,6 @@
 package br.com.vuttr;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -10,64 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.vuttr.domain.dto.ToolDTO;
-import br.com.vuttr.repository.ToolRepository;
 import br.com.vuttr.service.ToolServices;
 
 @SpringBootTest
-class VuttrApiApplicationTests {
+public class testListarTools {
 	
 	@Autowired
 	private ToolServices services;
 	
-	
 	/*
-	 * Teste para verificação do funcionamento da funcionalidade de Cadastro de ferrmantas(Tool)
+	 * Teste para verificar a funcionalidade de Listar as Ferramentas(Tools)
 	 */
+	
 	@Test
-	public void testeCadstroTool() {
+	public void testeListarTodasTools() {
 		
-		//*****Execução*****
+		//***Preparação do Ambiente***	
 		ToolDTO dto = services.save(this.carregarObjeto());
 		ToolDTO dto2 = services.save(this.carregarObjeto());
 		ToolDTO dto3 = services.save(this.carregarObjeto());
 		
-		//***Verificações***
-		assertNotNull(dto);
-		assertNotNull(dto2);
-		assertNotNull(dto3);
+		
+		//*****Verificação*****
 		assertTrue(services.findAll().size() == 3);
 		assertFalse(services.findAll().size() == 1);
+		assertFalse(services.findAll().size() == 2);
 		assertFalse(services.findAll().size() == 4);
+		
 	}
-
-		
-	/*
-	 * Teste para verificar o funcionamento da funcionalidade de deletar
-	 */
-	
-	/*
-	@Test
-	public void testeDeletarTool() {
-		
-		//***Preparação do ambiente***
-		ToolDTO dto = services.save(this.carregarObjeto());
-		ToolDTO dto2 = services.save(this.carregarObjeto());
-		ToolDTO dto3 = services.save(this.carregarObjeto());
-		
-		
-		//********Execução********
-		services.delete(2L);
-		
-		//***Verificações***
-		assertNull(dto2);
-		
-		
-	}*/
-	
-	
-	
-	
-	
 	
 	/*
 	 * Método criado para carregar objetos para preparação do ambiente de teste 
@@ -80,4 +48,5 @@ class VuttrApiApplicationTests {
 				description("All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.").
 				tags(tags).build();
 	}
+
 }
